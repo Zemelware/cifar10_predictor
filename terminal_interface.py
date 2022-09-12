0bbf0a5174aca97f6af5017cfa905ad770a0e4be
Ethan Zemelman <ethanzemelman@gmail.com>
2022-09-11 21:35:45 -0400

    Create a script to use the app from the terminal

---
+++ terminal_interface.py
@@ -0,0 +1,17 @@
+import classifier
+
+print("Welcome! This is an AI trained on the CIFAR10 dataset. You can classify images in any of the following categories:")
+print("airplane, automobile, bird, cat, deer, dog, frog, horse, ship, truck\n")
+
+path = input("Please enter the path to the image you want to classify: ")
+
+image = classifier.load_image(path)
+prediction = classifier.predict_image(image, return_probabilities=True)
+prediction = sorted(prediction, reverse=True)
+
+print("\n--------------------------------------------")
+print("Here are the probabilities for each class:")
+for i in range(len(prediction)):
+    print(f"{classifier.classes[i]}: {'{:.2%}'.format(prediction[i])}")
+
+print("The AI predicts that this image is a(n)", prediction[0])
