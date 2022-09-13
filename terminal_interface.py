bd1ea41f835937cbd7f354c005e83f1b9595a5f7
Ethan Zemelman <ethanzemelman@gmail.com>
2022-09-12 20:57:01 -0400

    Put everything in a loop to try again without restarting the script
    Also add functionality to accept the image path as a command line argument.

--- terminal_interface.py
+++ terminal_interface.py
@@ -1,26 +1,44 @@
+import sys
 import classifier
 
-print("Welcome! This is an AI trained on the CIFAR10 dataset. You can classify images in any of the following categories:")
+if len(sys.argv) > 2:
+    print("\033[91mYou've entered too many arguments. Please try again, with one argument as the filepath to the image "
+          "you would like the AI to classify.\033[0m")
+    sys.exit(1)
+
+print("Welcome! This is an AI trained on the CIFAR10 dataset. The AI can classify images in any of the following categories:")
 print("airplane, automobile, bird, cat, deer, dog, frog, horse, ship, truck\n")
 
 print("Make sure your image is a square for the best results!")
-path = input("Please enter the path to the image you want to classify: ")
 
-try:
-    image = classifier.load_image(path)
-except Exception as e:
-    print(e)
-    exit()
-prediction = classifier.predict_image(image, return_probabilities=True)
-prediction = sorted(prediction.items(), key=lambda x: x[1], reverse=True)
+first_time = True
+while True:
+    if len(sys.argv) == 1 or not first_time:
+        path = input("\nPlease enter the path to the image you want to classify: ")
+    elif len(sys.argv) == 2:
+        path = sys.argv[1]
 
-print("\n--------------------------------------------")
-print("Here are the probabilities for each class:")
-for i, (category, probability) in enumerate(prediction):
-    if i == 0: print('\033[92m', end='')  # Print the prediction in green
-    print(f"{category}: {'{:.2%}'.format(probability)}")
-    if i == 0: print('\033[0m', end='')
+    try:
+        image = classifier.load_image(path)
+    except Exception as e:
+        print(f"\n\033[91m{e}\033[0m")
+        first_time = False
+        continue
+    prediction = classifier.predict_image(image, return_probabilities=True)
+    prediction = sorted(prediction.items(), key=lambda x: x[1], reverse=True)
 
-# TODO: Put everything into a loop
+    print("\n--------------------------------------------")
+    print("Here are the probabilities for each class:")
+    for i, (category, probability) in enumerate(prediction):
+        if i == 0: print('\033[92m', end='')  # Print the prediction in green
+        print(f"{category}: {'{:.2%}'.format(probability)}")
+        if i == 0: print('\033[0m', end='')
+
+    print("--------------------------------------------\n")
+    choice = input("Do you want to classify another image? (y/n): ")
+    if choice.lower() == 'n':
+        break
+
+    first_time = False
+
 # TODO: Add a GUI to make this more user-friendly
-# TODO: Allow the user to specify the image as a command line argument
