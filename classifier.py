c6a7b7b16e7b5359d625d1731f3f45aa4767504e
Ethan Zemelman <ethanzemelman@gmail.com>
2022-09-27 16:45:09 -0400

    Save only the best set of weights instead of saving each checkpoint

--- classifier.py
+++ classifier.py
@@ -73,8 +73,6 @@ def create_model(learning_rate, num_classes):
     with tf.device('/CPU:0'):  # Fix M1 TensorFlow Metal bug
         data_augmentation = models.Sequential([
             layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=(32, 32, 3)),
-            # layers.experimental.preprocessing.RandomRotation(0.1),
-            # layers.experimental.preprocessing.RandomZoom(0.1),
             layers.experimental.preprocessing.RandomWidth(0.1),
             layers.experimental.preprocessing.RandomHeight(0.1),
         ])
@@ -124,32 +122,28 @@ classes = ["airplane", "automobile", "bird", "cat",
            "deer", "dog", "frog", "horse", "ship", "truck"]
 
 LEARNING_RATE = 0.001
-EPOCHS = 70
+EPOCHS = 1000
 BATCH_SIZE = 200
 
-training_num = 1
-while os.path.exists(f"weights/training_{training_num}"):
-    training_num += 1
-checkpoint_dir = f"weights/training_{training_num}"
-checkpoint_path = checkpoint_dir + "/cp-{epoch:03d}-{val_accuracy:.03f}.ckpt"
+weights_path = "best_weights/best_weights.ckpt"
+weights_dir = os.path.dirname(weights_path)
 
 model = create_model(LEARNING_RATE, len(classes))
-if os.path.exists(checkpoint_dir):
-    latest = tf.train.latest_checkpoint(checkpoint_dir)
-    model.load_weights(latest)
+if os.path.exists(weights_dir):
+    model.load_weights(weights_path)
 
 if __name__ == "__main__":
     # Create a callback that saves the model's best weights
-    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=weights_path,
                                                      monitor='val_accuracy',
-                                                     save_best_only=True,
                                                      save_weights_only=True,
+                                                     save_best_only=True,
                                                      mode='max')
 
     # Create an early stopping callback
     es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
-                                                   min_delta=0.001,
                                                    patience=10,
+                                                   verbose=1,
                                                    mode='min')
 
     X_train, y_train, X_test, y_test = preprocess_data()
